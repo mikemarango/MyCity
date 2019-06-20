f85f0e57af9855eee32162755cd295a9ff854cc6
Michael Marango <mike.marango@gmail.com>
2019-06-19 23:55:58 -0700

    created models

---
+++ City/City/Models/Attraction.cs
@@ -0,0 +1,11 @@
+﻿namespace City.Models
+{
+    public class Attraction
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int CityID { get; set; }
+        public Citi Citi { get; set; }
+    }
+}
---
+++ City/City/Models/Citi.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace City.Models
+{
+    public class Citi
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public ICollection<Attraction> Attractions { get; set; }
+    }
+}
